15b750ceb8002dc808f952b96539352a7fd95627
Nadia Puji Utami <nadiapujiutami@gmail.com>
2024-09-16 22:35:03 +0700

    init BE project

---
+++ simple-shop-api/src/main/java/com/naputami/simple_shop_api/SimpleShopApiApplication.java
@@ -0,0 +1,13 @@
+package com.naputami.simple_shop_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleShopApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleShopApiApplication.class, args);
+	}
+
+}
